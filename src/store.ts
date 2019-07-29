5559b46676866189f9e4735cb325f31315dfffa8
Jack Herrington <jherr@pobox.com>
2019-07-29 06:21:32 -0700

    Updates

--- src/store.ts
+++ src/store.ts
@@ -12,15 +12,21 @@ const startingCards = [
 
 export default new Vuex.Store({
   state: {
-    cards: startingCards.slice(),
+    cards: startingCards,
     table: new Table(startingCards),
   },
   mutations: {
     setCard(state, { index, card }) {
       state.cards[index] = card;
-      state.table = new Table(startingCards);
+      state.table = new Table(state.cards);
+    },
+    setupTable(state) {
+      state.table = new Table(state.cards);
     },
   },
   actions: {
+    runHand({ commit }) {
+      commit('setupTable');
+    },
   },
 });
